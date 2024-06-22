36b457b5a5ffa595b113d9e22ac41fd415e04787
Pedro <pedroaugustosilva166@gmail.com>
2024-06-21 22:44:34 -0300

    Criei a base do modulo web

---
+++ web.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Funcao que coleta os comentarios
+def getWeb(url, comments):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "lxml")
+    divContent = soup.find_all("div", class_="content")
+
+    for content_div in divContent:
+        i = 0
+        text_div = content_div.find("div", class_="text show-more__control")
+        if text_div:
+            comments.insert(i, text_div.text.strip())
+            i += 1
+    
+    return comments

db132a8079b51a0dade1a1fa42d864dbc8c0d90e
Pedro <pedroaugustosilva166@gmail.com>
2024-06-22 10:48:55 -0300

    Criei a funcao que coleta a media das avaliacos

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from math import ceil
 import requests
 
 # Funcao que coleta os comentarios
@@ -15,3 +16,26 @@ def getWeb(url, comments):
             i += 1
     
     return comments
+
+# Funcao que coleta a media da avaliacao
+def get_rates(url):
+    # Define a base de busca:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "lxml")
+    rates_base = soup.find_all("span", class_="rating-other-user-rating")
+
+    # Coleta todas avaliacoes no formato X/1O:
+    rates = []
+    for rate in rates_base:  
+        rate = rate.text.strip()
+        rates.append(rate)
+
+    # Calcula a media de todas as avaliacoes:
+    rates_sum = 0
+    for rate in rates:
+        rate = rate.split("/") # X/10 vira ["X", "10"]
+        rates_sum += int(rate[0])/len(rates)
+
+    rates_sum = ceil((rates_sum*10))/10 # ceil: funcao teto. Calcula a aproximacao de um decimal
+
+    return rates_sum

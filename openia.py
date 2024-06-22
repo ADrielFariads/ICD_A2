674f434bf72dff7bc5bcb62a23deed997e468311
Pedro <pedroaugustosilva166@gmail.com>
2024-06-22 19:42:36 -0300

    Fiz a funcao que pede insights para o chatgpt

--- openia.py
+++ openia.py
@@ -3,5 +3,15 @@ from openai import OpenAI
 prompt = []
 client = OpenAI(api_key="chave") # Inserir a chave aqui
 
-def addInfo(comments, movie):
-    return True # Comecar a funcao
+def addInfo(coment): #função que pede insights para a api openai
+    prompt = [{"role": "user",
+               "content": f"A seguir, você receberá um comentário referente a um filme. Classifique a opinião do usuário e diga em que aspectos o filme poderia melhorar.filme: saving prite ryan, {coment}"}]
+    client = OpenAI(api_key="")  # Inserir a chave aqui
+
+    response = client.chat.completions.create(
+        messages= prompt,
+        model= "gpt-3.5-turbo-0125",
+        max_tokens= 4000,
+        temperature= 0
+    )
+    return response.choices[0].message.content

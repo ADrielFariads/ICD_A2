63d530bf638af30befdfbf6c4a8027e341d3f492
Pedro <pedroaugustosilva166@gmail.com>
2024-06-21 22:37:20 -0300

    Criei a base do modulo main

---
+++ main.py
@@ -0,0 +1,22 @@
+from web import getWeb
+from openiamod import addInfo, prompt
+
+
+# Dicionario que contem o codigo especifico de um url para cada filme
+urls = {
+    "Saving Private Ryan": "tt0120815",
+    "Jaws": "tt0073195",
+    "Schindler's List": "tt0108052"
+}
+
+# Dicionario que contem os comntarios
+comments = {
+    "Saving Private Ryan": [],
+    "Jaws": [],
+    "Schindler's List": []
+}
+
+# Funcao que coleta os comentarios
+for url, url_Id in urls.items():
+    getWeb(f"https://www.imdb.com/title/{url_Id}/reviews/?ref_=tt_ql_2", comments[url])
+    addInfo(comments[url], url)

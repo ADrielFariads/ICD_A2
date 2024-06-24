890ee2f7aae1a18abf26ded36e09545b8962aa74
Pedro <pedroaugustosilva166@gmail.com>
2024-06-24 15:42:51 -0300

    Consertei a funcao principal que comunica com a openai

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from data import *
 
 for movie, url in urls.items():
     comments_base = wb.get_web(f"https://www.imdb.com/title/{url}/reviews/?ref_=tt_ql_2", comments[movie]) # Para cada link, a funcao pega todos os comentarios e adiciona a uma lista
-    for comment in comments_base:
-        positive_aspects[movie] = opai.get_positive_aspects(movie)
-        negative_aspects[movie] = opai.get_negative_aspects(movie)
+    for comment in comments_base: # Para cada comentario, a funcao pede a openai retorne os aspectos do filme contidos nele
+        positive_aspects[movie] += opai.get_positive_aspects(movie)
+        negative_aspects[movie] += opai.get_negative_aspects(movie)
+
     positive_aspects[movie] = opai.get_resume(positive_aspects[movie], "p")
     negative_aspects[movie] = opai.get_resume(negative_aspects[movie], "n")
     print(positive_aspects[movie])

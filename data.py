388dfd9198ed9ebdd33b57533c7d14b0dff8135d
Pedro <pedroaugustosilva166@gmail.com>
2024-06-24 15:24:24 -0300

    Criei os dicionarios do modulo data

---
+++ data.py
@@ -0,0 +1,97 @@
+# Dicionario que contem o codigo especifico de um url para cada filme
+urls = {
+    "A.I. Artificial Intelligence": "tt0212720",
+    "Minority Report": "tt0181689",
+    "Catch Me If You Can" : "tt0264464",
+    "The Terminal": "tt0362227",
+    "War of the Worlds": "tt0407304",
+    "Munich": "tt0408306",
+    "Indiana Jones and the Kingdom of the Crystal Skull": "tt0367882",
+    "The Adventures of Tintin": "tt0983193",
+    "War Horse": "tt1568911",
+    "Lincoln": "tt0443272",
+    "Bridge of Spies": "tt3682448",
+    "The BFG": "tt3691740",
+    "The Post": "tt6294822",
+    "Ready Player One": "tt1677720",
+    "West Side Story": "tt3581652",
+    "The Fabelmans": "tt14208870"
+}
+
+# Dicionario que contem os comntarios
+years = {
+    "A.I. Artificial Intelligence": "",
+    "Minority Report": "",
+    "Catch Me If You Can" : "",
+    "The Terminal": "",
+    "War of the Worlds": "",
+    "Munich": "",
+    "Indiana Jones and the Kingdom of the Crystal Skull": "",
+    "The Adventures of Tintin": "",
+    "War Horse": "",
+    "Lincoln": "",
+    "Bridge of Spies": "",
+    "The BFG": "",
+    "The Post": "",
+    "Ready Player One": "",
+    "West Side Story": "",
+    "The Fabelmans": ""
+}
+
+# Dicionario que contem os comntarios
+comments = {
+    "A.I. Artificial Intelligence": [],
+    "Minority Report": [],
+    "Catch Me If You Can" : [],
+    "The Terminal": [],
+    "War of the Worlds": [],
+    "Munich": [],
+    "Indiana Jones and the Kingdom of the Crystal Skull": [],
+    "The Adventures of Tintin": [],
+    "War Horse": [],
+    "Lincoln": [],
+    "Bridge of Spies": [],
+    "The BFG": [],
+    "The Post": [],
+    "Ready Player One": [],
+    "West Side Story": [],
+    "The Fabelmans": []
+}
+
+positive_aspects = {
+    "A.I. Artificial Intelligence": [],
+    "Minority Report": [],
+    "Catch Me If You Can" : [],
+    "The Terminal": [],
+    "War of the Worlds": [],
+    "Munich": [],
+    "Indiana Jones and the Kingdom of the Crystal Skull": [],
+    "The Adventures of Tintin": [],
+    "War Horse": [],
+    "Lincoln": [],
+    "Bridge of Spies": [],
+    "The BFG": [],
+    "The Post": [],
+    "Ready Player One": [],
+    "West Side Story": [],
+    "The Fabelmans": []
+}
+
+negative_aspects = {
+    "A.I. Artificial Intelligence": [],
+    "Minority Report": [],
+    "Catch Me If You Can" : [],
+    "The Terminal": [],
+    "War of the Worlds": [],
+    "Munich": [],
+    "Indiana Jones and the Kingdom of the Crystal Skull": [],
+    "The Adventures of Tintin": [],
+    "War Horse": [],
+    "Lincoln": [],
+    "Bridge of Spies": [],
+    "The BFG": [],
+    "The Post": [],
+    "Ready Player One": [],
+    "West Side Story": [],
+    "The Fabelmans": []
+}
